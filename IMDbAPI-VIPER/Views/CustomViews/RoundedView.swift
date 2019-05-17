f915008f9b3c8ed0e1907fb7411bbd702ea3f348
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-17 11:30:56 +0300

    creates rounded view class

---
+++ IMDbAPI-VIPER/Views/CustomViews/RoundedView.swift
@@ -0,0 +1,28 @@
+//
+//  RoundedView.swift
+//  IMDbAPI-VIPER
+//
+//  Created by Furkan Kurnaz on 17.05.2019.
+//  Copyright © 2019 Furkan Kurnaz. All rights reserved.
+//
+
+import UIKit
+
+class RoundedView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configureView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureView()
+    }
+    
+    func configureView() {
+        self.layer.cornerRadius = 10
+    }
+    
+}
