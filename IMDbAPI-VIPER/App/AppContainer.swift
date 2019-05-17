ce3d3d5e20062c03d68b945adf76fa4206cff8e3
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-17 11:28:56 +0300

    creates app layer

---
+++ IMDbAPI-VIPER/App/AppContainer.swift
@@ -0,0 +1,17 @@
+//
+//  AppContainer.swift
+//  IMDbAPI-VIPER
+//
+//  Created by Furkan Kurnaz on 17.05.2019.
+//  Copyright © 2019 Furkan Kurnaz. All rights reserved.
+//
+
+import Foundation
+
+let app = AppContainer()
+
+final class AppContainer {
+    
+    let router = AppRouter()
+//    let service = 
+}
---
+++ IMDbAPI-VIPER/App/AppRouter.swift
@@ -0,0 +1,24 @@
+//
+//  AppRouter.swift
+//  IMDbAPI-VIPER
+//
+//  Created by Furkan Kurnaz on 17.05.2019.
+//  Copyright © 2019 Furkan Kurnaz. All rights reserved.
+//
+
+import UIKit
+
+final class AppRouter {
+    let window: UIWindow
+    
+    init() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+    }
+    
+    func start() {
+//        let viewController = MovieListBuilder.make()
+//        let navigationController = UINavigationController(rootViewController: viewController)
+//        window.rootViewController = navigationController
+//        window.makeKeyAndVisible()
+    }
+}
