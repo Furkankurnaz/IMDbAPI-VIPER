2b72d30059a762af9e3115ce99d75b76ddd1a49f
Furkan Kurnaz <furkan.kurnaz@kgteknoloji.com>
2019-05-17 11:37:06 +0300

    creates search scene templates

---
+++ IMDbAPI-VIPER/Scenes/Search/SearchInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  SearchInteractor.swift
+//  IMDbAPI-VIPER
+//
+//  Created by Furkan Kurnaz on 17.05.2019.
+//  Copyright © 2019 Furkan Kurnaz. All rights reserved.
+//
+
+import Foundation
---
+++ IMDbAPI-VIPER/Scenes/Search/SearchRouter.swift
@@ -0,0 +1,9 @@
+//
+//  SearchRouter.swift
+//  IMDbAPI-VIPER
+//
+//  Created by Furkan Kurnaz on 17.05.2019.
+//  Copyright © 2019 Furkan Kurnaz. All rights reserved.
+//
+
+import Foundation
